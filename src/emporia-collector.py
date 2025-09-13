a5265187fb21f189b586709f747ee4b860db89f4
Jay Curtis <jaysuzi5@gmail.com>
2025-09-13 17:32:27 -0400

    enhancing logging

--- src/emporia-collector.py
+++ src/emporia-collector.py
@@ -1,23 +1,122 @@
-from datetime import datetime, timezone, timedelta
+import requests
+import yaml
 import os
+import traceback
+from datetime import datetime, timezone, timedelta
 from dotenv import load_dotenv
 from emporia.emporia import Emporia
-import requests
-import logging
+from framework.json_logger import JsonLogger
 
+# Define Globally.  This will be assigned after configuration is read
+json_logger = JsonLogger()
+API_URL = ""
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+def _load_config(path: str = "src/configuration/config.yaml") -> dict:
+    with open(path, "r") as f:
+        config = yaml.safe_load(f)  # safe_load ensures security
+    return config
 
-API_URL = "http://emporia.dev.svc.cluster.local:5001/api/v1/emporia/"
-API_SEARCH_URL = "http://emporia.dev.svc.cluster.local:5001/api/v1/emporia/search"
-API_DELETE_URL = "http://emporia.dev.svc.cluster.local:5001/api/v1/emporia/{id}"
 
 def _get_emporia_data(emporia, days_back):
-    return emporia.get_usage(days_back=days_back)
+    usages = []
+    return_code = 200
+    payload = {"days_back": days_back}
+    source_request = json_logger.source_request(source_component="Emporia", payload=payload)
+    try:
+        usages = emporia.get_usage(days_back=days_back)
+        if usages:
+            payload["usage_records"] = len(usages)
+    except Exception as ex:
+        return_code = 500
+        payload["usage_records"] = 0
+        stack_trace = traceback.format_exc()
+        message = "Exception collecting Emporia data"
+        data = {
+            "days_back": days_back
+        }
+        json_logger.message(message=message, exception=ex, stack_trace=stack_trace, source_request=source_request, data=data)
+
+    json_logger.source_response(source_request=source_request, return_code=return_code, payload=payload)
+    return return_code, usages
+
+def _get_local_data(instant: datetime) -> tuple:
+    return_code = 200
+    payload = {}
+    results = None
+    date_string = instant.isoformat(timespec="milliseconds").replace("+00:00", "Z")
+    payload["start_date"] = date_string
+    source_request = json_logger.source_request(source_component="emporia: Local Search", payload=payload)
+
+    try:
+        response = requests.post(API_URL+"search", json=payload)
+        payload['status_code'] = str(response.status_code)
+        if response.status_code == 200:
+            results = response.json()
+            if results:
+                payload['result_records'] = len(results)
+        else:
+            return_code = 500
+            payload['result_records'] = 0
+            message = "Exception collecting local data: Non-200 status code"
+            data = {
+                "instant": instant
+            }
+            json_logger.message(message=message, source_request=source_request, data=data)
+    except requests.RequestException as ex:
+        return_code = 500
+        payload['result_records'] = 0
+        stack_trace = traceback.format_exc()
+        message = "Exception collecting local data"
+        data = {
+            "instant": instant
+        }
+        json_logger.message(message=message, exception=ex, stack_trace=stack_trace,
+                            source_request=source_request, data=data)
+
+    json_logger.source_response(source_request=source_request, return_code=return_code, payload=payload)
+    return return_code, results
+
+def _delete_local_data(response) -> tuple:
+    payload = {}
+    return_code = 200
+    total_records = len(response)
+    deleted = 0
+    errors = 0
+    payload["total_records"] = total_records
+    source_request = json_logger.source_request(source_component="emporia: Local Delete", payload=payload)
+    try:
+        for record in response:
+            record_id = str(record['id'])
+            response = requests.delete(API_URL + record_id)
+            if response.status_code == 200:
+                deleted += 1
+            else:
+                return_code = 500
+                errors += 1
+                message = "Exception deleting local data: Non-200 status code"
+                data = {
+                    "record_id": record_id
+                }
+                json_logger.message(message=message, source_request=source_request, data=data)
+    except requests.RequestException as ex:
+        return_code = 500
+        errors = total_records - deleted
+        stack_trace = traceback.format_exc()
+        message = "Exception deleting local data"
+        json_logger.message(message=message, exception=ex, stack_trace=stack_trace, source_request=source_request)
+    payload['deleted'] = deleted
+    payload['errors'] = errors
+    json_logger.source_response(source_request=source_request, return_code=return_code, payload=payload)
+    return return_code, deleted, errors
 
 def _load_emporia_data(usages):
-    inserted_records = 0
+    payload = {}
+    return_code = 200
+    total_records = len(usages)
+    inserted = 0
+    errors = 0
+    payload["total_records"] = total_records
+    source_request = json_logger.source_request(source_component="Emporia", payload=payload)
     for usage in usages:
         # Convert 'instant' datetime to ISO 8601 string with UTC 'Z'
         instant_iso = usage['instant'].astimezone(timezone.utc).isoformat().replace("+00:00", "Z")
@@ -32,69 +131,102 @@ def _load_emporia_data(usages):
             "unit": usage.get("unit", ""),
             "percentage": usage.get("percentage", 0)
         }
-
         try:
             response = requests.post(API_URL, json=payload)
-            if response.status_code in (200, 201):
-                inserted_records += 1
+            if response.status_code == 200:
+                inserted += 1
             else:
-                logger.error(f"emporia-collector._load_emporia_data: Failed: : {response.status_code}, "
-                              f"{response.text}")
-        except requests.RequestException as e:
-            logger.error(f"emporia-collector._load_emporia_data: Exception: {e}")
-    return inserted_records
-
-def _get_local_data(date_string):
-    payload = {
-        "start_date": date_string
-    }
-    try:
-        response = requests.post(API_SEARCH_URL, json=payload)
-        if response.status_code in (200, 201):
-            return response.json()
-        else:
-            logger.error(f"emporia-collector._get_local_data: Failed: : {response.status_code}, {response.text}")
-    except requests.RequestException as e:
-        logger.error(f"emporia-collector._get_local_data: Exception: {e}")
-
-
-def _delete_local_data(record_id):
-    url = API_DELETE_URL
-    url = url.format(id=record_id)
-    try:
-        response = requests.delete(url)
-        if response.status_code in (200, 201):
-            pass
-        else:
-            logger.error(f"emporia-collector._delete_local_data: Failed to delete")
-    except requests.RequestException as e:
-        logger.error(f"emporia-collector._delete_local_data: Exception: {e}")
+                return_code = 500
+                errors += 1
+                message = "Exception inserting local data: Non-200 status code"
+                data = {
+                    "name": name
+                }
+                json_logger.message(message=message, source_request=source_request, data=data)
+        except requests.RequestException as ex:
+            return_code = 500
+            errors = total_records - inserted
+            stack_trace = traceback.format_exc()
+            message = "Exception deleting local data"
+            json_logger.message(message=message, exception=ex, stack_trace=stack_trace, source_request=source_request)
+    payload['inserted'] = inserted
+    payload['errors'] = errors
+    json_logger.source_response(source_request=source_request, return_code=return_code, payload=payload)
+    return return_code, inserted, errors
 
 
-def _load_day(emporia, days_back=0):
-    total_records = 0
+def _load_day(emporia, days_back=0) -> tuple:
     instant = datetime.now(timezone.utc) - timedelta(days=days_back)
-    logger.info(f'"emporia-collector._load_day: collecting data starting at: {datetime.now()} for: {instant}')
-    date_string = instant.isoformat(timespec="milliseconds").replace("+00:00", "Z")
-    usages = _get_emporia_data(emporia, days_back)
-    logger.info(f'emporia-collector._load_day: {len(usages)} usages returned from Emporia')
-    if usages and len(usages) > 0:
+    total_records = 0
+    total_errors = 0
+    deleted = 0
+    return_code, usages = _get_emporia_data(emporia, days_back)
+    if return_code == 200 and usages and len(usages) > 0:
         # First remove the local data
-        response = _get_local_data(date_string)
-        for record in response:
-            _delete_local_data(record['id'])
-        # Load the local data
-        records_inserted = _load_emporia_data(usages)
-        total_records += records_inserted
-    logger.info(f'"emporia-collector._load_day: finished: {datetime.now()} collected: {total_records} records')
+        return_code, response = _get_local_data(instant)
+        if return_code == 200:
+            return_code, deleted, errors = _delete_local_data(response)
+            total_errors += errors
+            if return_code == 200:
+                # Load the local data
+                return_code, records_inserted, errors = _load_emporia_data(usages)
+                total_records += records_inserted
+                total_errors += errors
+
+    return return_code, instant, total_records, deleted, total_errors
 
 
 def main():
+    global json_logger, API_URL
+    payload= {}
+    response_return_code = 200
+    total_records = 0
+    total_deleted = 0
+    total_errors = 0
     load_dotenv()
+    API_URL = os.getenv("API_URL")
+    config = _load_config()
+    json_logger.define(config)
+    json_logger.request()
+    # Create the Emporia object that will be used to call the external Emporia APIs
     emporia = Emporia(os.getenv("USERNAME"), os.getenv("PASSWORD"), os.getenv("CLIENT_ID"))
-    _load_day(emporia, days_back=1)
-    _load_day(emporia, days_back=0)
 
+    # Call for yesterday's data and load it to PostgreSQL
+    return_code, instant, records, deleted, errors = _load_day(emporia, days_back=1)
+    if return_code > response_return_code:
+        response_return_code = return_code
+    total_records += records
+    total_deleted += deleted
+    total_errors += errors
+    payload['details'] = []
+    payload["details"].append(
+        {
+            'instant': instant,
+            'records': records,
+            'deleted': deleted,
+            'errors': errors
+        })
+
+    # Call for today's data and load it to PostgreSQL
+    return_code, instant, total_records, deleted, errors = _load_day(emporia, days_back=0)
+    if return_code > response_return_code:
+        response_return_code = return_code
+    total_records += records
+    total_deleted += deleted
+    total_errors += errors
+    payload["details"].append(
+        {
+            'instant': instant,
+            'records': records,
+            'deleted': deleted,
+            'errors': errors
+        })
+
+    payload['total_records'] = total_records
+    payload['total_deleted'] = total_deleted
+    payload['total_errors'] = total_errors
+
+    json_logger.response(payload=payload, return_code=response_return_code)
 
 if __name__ == "__main__":
     main()
---
+++ src/framework/json_logger.py
@@ -0,0 +1,147 @@
+from datetime import datetime, timezone
+import inspect
+import logging
+import uuid
+
+
+class JsonLogger:
+    def __init__(self):
+        self._config = None
+        self._transaction_id = None
+        self._logger = None
+        self._request = {}
+
+    def define(self, config: dict) -> None:
+        validate_configuration(config)
+        self._config = config
+        self._transaction_id = uuid.uuid4()
+        self._logger = logging.getLogger(self._config['logging_info']['component'])
+        log_level_str = self._config['logging_info']['level'].upper()
+        log_level = getattr(logging, log_level_str, logging.INFO)
+        logging.basicConfig(level=log_level)
+
+    def request(self, payload: dict = None) -> None:
+        log_message = {
+            "level": "INFO",
+            "event": "Request",
+            "component": self._config['logging_info']['component'],
+            "component_type": self._config['logging_info']['component_type'],
+            "timestamp": datetime.now(timezone.utc).isoformat(),
+            "transaction_id": self._transaction_id,
+        }
+        if payload:
+            log_message["payload"] = payload
+        self._request = log_message
+        self._logger.info(log_message)
+
+    def response(self, return_code: int = 200, payload: dict = None) -> None:
+        request_time = self._request["timestamp"]
+        response_time = datetime.now(timezone.utc).isoformat()
+        time_difference = datetime.fromisoformat(response_time) - datetime.fromisoformat(request_time)
+        duration = time_difference.total_seconds()
+        log_message = {
+            "level": "INFO",
+            "event": "Response",
+            "component": self._config['logging_info']['component'],
+            "component_type": self._config['logging_info']['component_type'],
+            "timestamp": response_time,
+            "transaction_id": self._transaction_id,
+            "duration": duration,
+            "return_code": return_code
+        }
+        if payload:
+            log_message["payload"] = payload
+        self._logger.info(log_message)
+
+    def source_request(self, source_component: str, payload: dict = None) -> dict:
+        log_message = {
+            "level": "INFO",
+            "event": "SourceRequest",
+            "component": self._config['logging_info']['component'],
+            "component_type": self._config['logging_info']['component_type'],
+            "source_component": source_component,
+            "timestamp": datetime.now(timezone.utc).isoformat(),
+            "transaction_id": self._transaction_id,
+            "source_transaction_id": uuid.uuid4()
+        }
+        if payload:
+            log_message["payload"] = payload
+        self._logger.info(log_message)
+        return log_message
+
+    def source_response(self, source_request: dict, return_code: int = 200, payload: dict = None) -> None:
+        source_request_time = source_request["timestamp"]
+        source_response_time = datetime.now(timezone.utc).isoformat()
+        time_difference = datetime.fromisoformat(source_response_time) - datetime.fromisoformat(source_request_time)
+        duration = time_difference.total_seconds()
+        log_message = {
+            "level": "INFO",
+            "event": "SourceResponse",
+            "component": self._config['logging_info']['component'],
+            "component_type": self._config['logging_info']['component_type'],
+            "source_component": source_request["source_component"],
+            "timestamp": source_response_time,
+            "transaction_id": self._transaction_id,
+            "source_transaction_id": source_request["source_transaction_id"],
+            "duration": duration,
+            "return_code": return_code
+        }
+        if payload:
+            log_message["payload"] = payload
+        self._logger.info(log_message)
+
+
+    def message(self, message:str, exception: Exception = None, stack_trace: str = None, data: dict = None,
+                source_request: dict = None, error: bool = False, debug: bool = False) -> None:
+        log_message = {
+            "message": message,
+            "component": self._config['logging_info']['component'],
+            "component_type": self._config['logging_info']['component_type'],
+            "timestamp": datetime.now(timezone.utc).isoformat(),
+            "transaction_id": self._transaction_id
+        }
+        if source_request:
+            log_message["source_component"] = source_request["source_component"]
+            log_message["source_transaction_id"] = source_request["source_transaction_id"]
+        if data:
+            log_message["data"] = data
+        else:
+            log_message["data"] = {}
+        if exception:
+            log_message["data"]["exception"] = str(exception)
+        if stack_trace:
+            log_message["stack_trace"] = stack_trace
+
+        # Log class and function if the class exist, otherwise just the method
+        try:
+            caller_frame = inspect.stack()[1].frame
+            self_obj = caller_frame.f_locals.get('self', None)
+            if self_obj:
+                log_message["method"] = type(self_obj).__name__ + caller_frame.f_code.co_name
+            else:
+                log_message["method"] = caller_frame.f_code.co_name
+        except: # Don't want the logging to throw an exception
+            pass
+
+        # Do the actual logging
+        if exception or error:
+            self._logger.error(log_message)
+        elif debug:
+            self._logger.debug(log_message)
+        else:
+            self._logger.info(log_message)
+
+
+def validate_configuration(config:dict) -> None:
+    if config is None:
+        raise ValueError("Configuration is missing or could not be loaded.")
+    if not isinstance(config, dict):
+        raise TypeError("Configuration must be a dictionary.")
+    if "logging_info" not in config:
+        raise ValueError("Configuration is missing logging_info.")
+    if "level" not in config['logging_info']:
+        raise ValueError("Configuration is missing level within logging_info.")
+    if "component" not in config['logging_info']:
+        raise ValueError("Configuration is missing component within logging_info.")
+    if "component_type" not in config['logging_info']:
+        raise ValueError("Configuration is missing component_type within logging_info.")
